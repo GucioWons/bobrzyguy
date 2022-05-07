6c47ad17f64237cbac914f8c90c10a8ca48814ea
GucioWons <wgutek1@wp.pl>
2022-05-07 15:17:14 +0200

    Requests model

--- Team/admin.py
+++ Team/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from Team.models import Team
+from Team.models import Team, Request
 
 admin.site.register(Team)
+admin.site.register(Request)
--- Team/models.py
+++ Team/models.py
@@ -24,3 +24,19 @@ class Team(models.Model):
 
     def get_leave_url(self):
         return reverse('cal:leave-view', kwargs={'my_id': self.id})
+
+class Request(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    user_from = models.ForeignKey(AppUser, on_delete=models.CASCADE, related_name="user_from")
+    user_to = models.ForeignKey(AppUser, on_delete=models.CASCADE, related_name="user_to")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_expired = models.DateTimeField()
+
+    def __str__(self):
+        return (user_to.email + " to " + team.name)
+
+    def get_accept_url(self):
+        return reverse('team:accept-view', kwargs={'my_id': self.id})
+
+    def get_decline_url(self):
+        return reverse('team:decline-view', kwargs={'my_id': self.id})
